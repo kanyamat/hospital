b1755bd62465d9bc750dd6d0f489d8d433a476ef
kanyamat <kanyamat003@gmail.com>
2018-01-04 14:18:43 +0700

    d

--- chatbot.php
+++ chatbot.php
@@ -1,1 +1,61 @@
 <?php
+$access_token = '2MD4waeOjAyBl8vV/r60JwXzouAjGDB5n6QFwLrfRVLXSlf0nfkIA861nwGiGYCTMo9zKsd0fAFqoUb+8E0j1FL6tioXbgrXusCgfacGVtxwbtD4n0GhuRSl0rfkt0VCVsgremd9z3nXhEMdGn5ZJgdB04t89/1O/w1cDnyilFU=';
+
+
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+$curr_years = date("Y");
+$curr_y = ($curr_years+ 543);
+$_msg = $events['events'][0]['message']['text'];
+$user = $events['events'][0]['source']['userId'];
+$user_id = pg_escape_string($user);
+$u = pg_escape_string($_msg);  
+$check_q = pg_query($dbconn,"SELECT seqcode, sender_id ,updated_at  FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+                while ($row = pg_fetch_row($check_q)) {
+                  echo $seqcode =  $row[0];
+                  echo $sender = $row[2]; 
+                } 
+// $check_user = pg_query($dbconn,"SELECT*FROM users  WHERE $user_id  = '{$user_id}' ");
+//****************ทดสอบ
+       // $d = date("D");
+       // $h = date("H:i");
+//****************ทดสอบ จบ
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+ // Loop through each event
+ foreach ($events['events'] as $event) {
+  // Reply only when message sent is in 'text' format
+  // if ($event['message']['text'] == "ต้องการผู้ช่วย") {
+
+ if (strpos($_msg, 'hello') !== false || strpos($_msg, 'สวัสดี') !== false || strpos($_msg, 'ต้องการผู้ช่วย') !== false) {
+
+      $replyToken = $event['replyToken'];
+      $text = "สวัสดีค่ะ คุณสนใจมีผู้ช่วยใช่ไหม";
+      // $messages = [
+      //   'type' => 'text',
+      //   'text' => $text
+      // ];
+        $messages = [
+       'type' => 'template',
+        'altText' => 'this is a confirm template',
+        'template' => [
+            'type' => 'confirm',
+            'text' => $text ,
+            'actions' => [
+                [
+                    'type' => 'message',
+                    'label' => 'สนใจ',
+                    'text' => 'สนใจ'
+                ],
+                [
+                    'type' => 'message',
+                    'label' => 'ไม่สนใจ',
+                    'text' => 'ไม่สนใจ'
+                ],
+            ]
+        ]
+    ];
+
+####################################  insert data to sequentsteps   ####################################
+ $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0004','','0005','0',NOW(),NOW())") or die(pg_errormessage());

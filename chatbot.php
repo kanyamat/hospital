d97daaf904e4cb10bd222d5f3ce7f7e2b7ed1648
kanyamat <kanyamat003@gmail.com>
2018-01-05 10:55:35 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -70,13 +70,42 @@ $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseq
                   echo $question = $row[1]; 
                 }   
 
-                 $replyToken = $event['replyToken'];
-                 $messages = [
-                        'type' => 'text',
-                        'text' =>  $question
-                      ];
+                 // $replyToken = $event['replyToken'];
+                 // $messages = [
+                 //        'type' => 'text',
+                 //        'text' =>  $question
+                 //      ];
  
-
+                  $messages = [
+                    'type': 'template',
+                    'altText': 'This is a buttons template',
+                    'template': {
+                        'type': 'buttons',
+                        'thumbnailImageUrl': 'https://example.com/bot/images/image.jpg',
+                        'imageAspectRatio': 'rectangle',
+                        'imageSize': 'cover',
+                        'imageBackgroundColor': '#FFFFFF',
+                        'title': 'Menu',
+                        'text': 'Please select',
+                        'actions': [
+                            {
+                              'type': 'postback',
+                              'label': 'Buy',
+                              'data': 'action=buy&itemid=123'
+                            },
+                            {
+                              'type': 'postback',
+                              'label': 'Add to cart',
+                              'data': 'action=add&itemid=123'
+                            },
+                            {
+                              'type': 'uri',
+                              'label': 'View detail',
+                              'uri': 'http://example.com/page/123'
+                            }
+                        ]
+                    }
+                  ];
                  // $replyToken = $event['replyToken'];
                  // $messages = [
                  //        'type' => 'text',

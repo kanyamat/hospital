1eb8dc3e3dbf728d4b52852d0c2991b16f665c9d
kanyamat <kanyamat003@gmail.com>
2018-01-08 10:17:47 +0700

    1

--- chatbot.php
+++ chatbot.php
@@ -257,7 +257,7 @@ $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseq
                   ];
 
 
-$q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0006','','0011','0',NOW(),NOW())") or die(pg_errormessage());
+$q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0006','','0000','0',NOW(),NOW())") or die(pg_errormessage());
 
  // Make a POST Request to Messaging API to reply to sender
          $url = 'https://api.line.me/v2/bot/message/reply';

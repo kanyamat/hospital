ae17144b8c147ecf4b304db2a9209434f3b1561c
kanyamat <kanyamat003@gmail.com>
2018-01-08 14:22:04 +0700

    i

--- chatbot.php
+++ chatbot.php
@@ -313,7 +313,7 @@ $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseq
                               ],
                           ]
                       ]
-                  ] 
+                  ]; 
 $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','0008','','0000','0',NOW(),NOW())") or die(pg_errormessage());
                    
 ########################################################################################################################################################

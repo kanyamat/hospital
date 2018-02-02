6d474d64b2ab3c5bd0655bb832fe59e0ac3a9126
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-02-02 13:44:07 +0700

    g

--- chatbot.php
+++ chatbot.php
@@ -2,7 +2,11 @@
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<X8ZPAwTyBxGjoLM1gelIBKpSXjMlmHOVUyRxe5K0suVS5D3BoPdl2OZZkPRiQih5Mo9zKsd0fAFqoUb+8E0j1FL6tioXbgrXusCgfacGVty5mH62yH0De2TsPUUAb53pTWzsnLMTUnI0cM96J7oY0AdB04t89/1O/w1cDnyilFU=>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<929b61468a8a14c77a37c2e124aa7183>']);
-$response = $bot->replyText('<reply token>', 'hello!');
+
+$replyToken  = $events['events'][0]['replyToken'];
+
+$response = $this->bot->replyText($replyToken, 'hello!');
+//$response = $bot->replyText('<reply token>', 'hello!');
 
 
 
